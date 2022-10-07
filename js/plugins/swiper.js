8d3055a5e0f6db1673a44e9e7604fa9f8bf78f31
Andrey <82231196+Badebum@users.noreply.github.com>
2022-10-07 10:12:46 +0300

    new

---
+++ js/plugins/swiper.js
@@ -0,0 +1,60 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const swiperSpecialists = new Swiper(".swiper__specialists", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 800,
+    centerInsufficientSlides: false,
+    loopedSlides: 2,
+    loop: true,
+    simulateTouch: true,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1025: {
+        slidesPerView: 3,
+        loop: false,
+
+      },
+
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const swiperEquipment = new Swiper(".swiper__equipment", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 800,
+    centerInsufficientSlides: false,
+    loopedSlides: 2,
+    loop: true,
+    simulateTouch: true,
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      // 1025: {
+      //     slidesPerView: 3,
+      //     direction: "vertical",
+      //     spaceBetween: 38,
+      //     loopedSlides: 4,
+      //     loop: false,
+      // },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
